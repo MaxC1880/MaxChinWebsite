0ed38554566db646b6f8eb1798a8e03dc92c7bc5
Maxwell Chin <maximum49er@gmail.com>
2023-03-30 11:31:57 -0400

    Removed unnecessary education

--- pofology/src/data/educations.ts
+++ pofology/src/data/educations.ts
@@ -8,18 +8,4 @@ export const educations: Education[] = [
     endDate: 'May 2025',
     description: 'Currently pursuing a double major at UMD, hoping to finish in the spring of 2025.',
   },
-  {
-    degree: 'Master of Science',
-    school: 'University of California',
-    startDate: 'Jan 2019',
-    endDate: 'Dec 2021',
-    description: 'Lorem ipsum dolor sit amet quo ei simul congue exerci ad nec admodum perfecto.',
-  },
-  {
-    degree: 'Doctor of Philosophy',
-    school: 'University of California',
-    startDate: 'Jan 2022',
-    endDate: 'Present',
-    description: 'Lorem ipsum dolor sit amet quo ei simul congue exerci ad nec admodum perfecto.',
-  },
 ];
